4326d0778edba7bcf610e6530a4bf7666d53565c
kurshit <kukreja.kurshit@gmail.com>
2019-01-22 21:18:25 +0530

    correcting commit

--- src/main/java/com/delloite/awayday/controller/ScheduleController.java
+++ src/main/java/com/delloite/awayday/controller/ScheduleController.java
@@ -1,6 +1,7 @@
 package com.delloite.awayday.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,11 +15,13 @@ public class ScheduleController {
 	@Autowired
 	private ScheduleService scheduleService;
 	
+	@Value("${fileName:}")
+	private String filePathAndName;
+	
 	@RequestMapping(value = "/getschedule/{totalTeams}", method = RequestMethod.GET)
 	public String getSchedule(@PathVariable("totalTeams") int totalTeams) {
-		
-		String fileName = "./activities.txt";
-		scheduleService.parseActivities(fileName);
+	
+		scheduleService.parseActivities(filePathAndName);
 		scheduleService.createTeams(totalTeams);
 		scheduleService.createSchedule(totalTeams);
 		
